17cf3fe2e6a436cd5f7dd6197379c05bd13a1f70
Collin <c_orear@icloud.com>
2017-04-03 21:39:01 +0000

    Added pages and skeleton for navigation.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  
+  # Backend code for landing page
+  def index
+  end
+
+  # Backend code for default home page
+  def home
+  end
+
+  # Backend code for viewing a profile page
+  def profile
+  end
+
+  # Backend code for exploring profiles
+  def explore
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  
+  # Define routes for navigating to pages
+  root 'pages#index'
+  # Sets default page ^
+  
+  get 'home' => 'pages#home'
+  get 'profile' => 'pages#profile'
+  get 'explore' => 'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
